6f3b1b328074513cec98927ae6ad4b6b14d1c738
NNesh <mullruslan@yandex.ru>
2017-07-20 16:33:22 +0500

    Added export file

---
+++ module.js
@@ -0,0 +1,5 @@
+module.exports = {
+  Component: require('./lib/component'),
+  ElementsManager: require('./lib/elements-manager'),
+  InterfaceManager: require('./lib/interface-manager')
+}
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function () {
-  const ElementsManager = require('./lib/elements-manager');
+  const { ElementsManager } = require('./module');
 
   let elManager = new ElementsManager('content');
 }).call(this);
