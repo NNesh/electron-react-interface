a8fdf63188d5bdb7c5579399efc94ca8d294925b
NNesh <mullruslan@yandex.ru>
2017-07-20 16:08:15 +0500

    Added id of instance

--- index.js
+++ index.js
@@ -5,11 +5,23 @@
   const url = require('url');
   const path = require('path');
 
+  const Component = require('./lib/component');
+
 
   let win;
 
   function createWindow() {
     win = new BrowserWindow({width: 800, height: 640});
+
+    win.on('closed', () => {
+      win = null;
+    });
+    win.on('focus', () => {
+      let component1 = new Component();
+      let component2 = new Component();
+      console.log('Component 1: %s', component1.id);
+      console.log('Component 2: %s', component2.id);
+    });
 
     win.loadURL(url.format({
       pathname: path.join(__dirname, 'index.html'),
@@ -18,12 +30,6 @@
     }));
 
     win.webContents.openDevTools();
-
-    win.on('closed', () => {
-      win = null;
-    });
-
-    console.log('created window');
   }
 
   app.on('ready', () => {
